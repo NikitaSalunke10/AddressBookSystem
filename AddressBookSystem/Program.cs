92fbaaa0692f4e6e565e4bd6b545325de38ccc0d
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-22 16:36:42 +0530

    Add contacts program is added

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -2,8 +2,6 @@
 
 Console.WriteLine("Welcome to AddressBook Program.");
 AddressBook addressBook = new AddressBook(); // creating the object of AddressBook class
-//addressBook.addContact(); // calling this method to add the contact
-//addressBook.listContact(); //calling this method to print the list of contacts
 int option = 0;
 while(option != 3) //while loop is used so user can enter option of which task to perform and it will execute till it get value 3
 {
