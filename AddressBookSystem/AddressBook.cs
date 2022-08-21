2258a9518f788a35e88f7cdb2d1ae9576dc73b02
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-21 23:51:52 +0530

    add and display unique contacts added

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -1,7 +1,5 @@
-﻿using CsvHelper;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -199,7 +197,6 @@ namespace AddressBookSystem
                 if(People.Contains(person)) 
                 {
                     dict.Add(firstName, People); // it will add the unique contact only and not the duplicate
-                    break;
                 }
             }
         }
@@ -211,160 +208,6 @@ namespace AddressBookSystem
                 {
                     Console.WriteLine("-------------------------------------------");
                     printContact(contact);
-                }
-            }
-        }
-        public void SearchContact() // this method is used to search a contact based on city or state
-        {
-            Console.WriteLine("-------------------------------------------");
-            Console.WriteLine("Enter the City or State to search contact: ");
-            string result = Console.ReadLine();
-            foreach(Person person in People.FindAll(e => e.City.Equals(result) || e.State.Equals(result)))
-            {
-                Console.WriteLine("-------------------------------------------");
-                printContact(person);
-            }
-        }
-        public void ViewPersonByCityState() // this method is used to create dictionary of city and state wise
-        {
-            Dictionary<string, List<Person>> CityAddressBook = new Dictionary<string, List<Person>>();
-            Dictionary<string, List<Person>> StateAddressBook = new Dictionary<string, List<Person>>();
-            Console.WriteLine("Press 1 if want to search by City or Press 2 if want to search by state");
-            int choice = Convert.ToInt32(Console.ReadLine());
-            string city, state;
-            switch (choice)
-            {
-                case 1:
-                    Console.WriteLine("-------------------------------------------");
-                    Console.WriteLine("Enter the city you want to search: ");
-                    city = Console.ReadLine();
-                    CityAddressBook[city] = new List<Person>();
-                    foreach(Person person in People.FindAll(e => e.City.Equals(city)))
-                    {
-                        CityAddressBook[city].Add(person);
-                    }
-                    foreach (var person in CityAddressBook) // it will print CityAddressBood dictionary
-                    {
-                        foreach (var item in person.Value)
-                        {
-                            Console.WriteLine("-------------------------------------------");
-                            printContact(item);
-                        }
-                    }
-                    Console.WriteLine("-------------------------------------------");
-                    Console.WriteLine("Total count of people at {0} city: {1}", city, CityAddressBook[city].Count); // this will print the city and count of contacts in that same city
-                    break;
-                case 2:
-                    Console.WriteLine("-------------------------------------------");
-                    Console.WriteLine("Enter the state you want to search: ");
-                    state = Console.ReadLine();
-                    StateAddressBook[state] = new List<Person>();
-                    foreach (Person person in People.FindAll(e => e.State.Equals(state)))
-                    {
-                        StateAddressBook[state].Add(person);
-                    }
-                    foreach (var person in StateAddressBook)// it will print StateAddressBood dictionary
-                    {
-                        foreach (var item in person.Value)
-                        {
-                            Console.WriteLine("-------------------------------------------");
-                            printContact(item);
-                        }
-                    }
-                    Console.WriteLine("-------------------------------------------");
-                    Console.WriteLine("Total count of people at {0} state: {1}", state, StateAddressBook[state].Count);// this will print the state and count of contacts in that same state
-                    break;
-                default:
-                    Console.WriteLine("Wrong input");
-                    break;
-            }
-        }
-        public void SortContacts() // this method is used to display sorted contacts
-        {
-            Console.WriteLine("-------------------------------------------");
-            Console.WriteLine("Press 1 to Sort by Name.\nPress 2 to Sort by City");
-            Console.WriteLine("Press 3 to Sort by State.\nPress 4 to Sort by ZipCode");
-            Console.WriteLine("Please enter the option: ");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
-            {
-                case 1:
-                    foreach (Person person in People.OrderBy(name => name.FirstName)) // OrderBy is used to sort the contacts by first names
-                    {
-                        Console.WriteLine("-------------------------------------------");
-                        printContact(person);
-                    }
-                    break;
-                case 2:
-                    foreach (Person person in People.OrderBy(city => city.City)) // OrderBy is used to sort the contacts by City
-                    {
-                        Console.WriteLine("-------------------------------------------");
-                        printContact(person);
-                    }
-                    break;
-                case 3:
-                    foreach (Person person in People.OrderBy(state => state.State)) // OrderBy is used to sort the contacts by state
-                    {
-                        Console.WriteLine("-------------------------------------------");
-                        printContact(person);
-                    }
-                    break;
-                case 4:
-                    foreach (Person person in People.OrderBy(code => code.ZipCode)) // OrderBy is used to sort the contacts by zip code
-                    {
-                        Console.WriteLine("-------------------------------------------");
-                        printContact(person);
-                    }
-                    break;
-                default :
-                    Console.WriteLine("Wrong input");
-                    break ;
-            }
-        }
-        public void ReadWriteFile() // this method is used to write and read the contacts
-        {
-            string path = @"C:\Users\LENOVO\source\repos\AddressBookSystem\AddressBookSystem\Contacts.txt"; // path of the file is stored in path
-            using(StreamWriter sw = new StreamWriter(path)) 
-            {
-                foreach(Person person in People)//foreach loop is used to write all contacts in file
-                {
-                    sw.WriteLine("-------------------------------------------");
-                    sw.WriteLine("Full name : " + person.FirstName + " " + person.LastName);
-                    sw.WriteLine("Mobile number : " + person.PhoneNumber);
-                    sw.WriteLine("Email ID : " + person.Email);
-                    sw.WriteLine("Address : " + person.Address);
-                    sw.WriteLine("City : " + person.City);
-                    sw.WriteLine("State : " + person.State);
-                    sw.WriteLine("Zipcode : " + person.ZipCode);
-                }
-                sw.Close();
-                Console.WriteLine(File.ReadAllText(path)); // all the contacts in the file are displayed
-            }
-        }
-        public void ReadWriteCSVFile()//this method is used to write and read contacts from csv file
-        {
-            string path1 = @"C:\Users\LENOVO\source\repos\AddressBookSystem\AddressBookSystem\AddressBook.csv"; //path of csv file is stored in path
-            Console.WriteLine("Writing contacts in CSV file");
-            using (var writer = new StreamWriter(path1))
-            using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
-            {
-                csvExport.WriteRecords(People); //contacts are written in csv file
-            }
-            using (var reader = new StreamReader(path1))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                var records = csv.GetRecords<Person>().ToList(); // contacts from csv file are converted in list and stored in records
-                Console.WriteLine("Reading contacts from CSV file");
-                foreach (Person person in records)//foreach loop is used to print all contacts
-                {
-                    Console.WriteLine("-------------------------------------------");
-                    Console.WriteLine("Full name : " + person.FirstName + " " + person.LastName);
-                    Console.WriteLine("Mobile number : " + person.PhoneNumber);
-                    Console.WriteLine("Email ID : " + person.Email);
-                    Console.WriteLine("Address : " + person.Address);
-                    Console.WriteLine("City : " + person.City);
-                    Console.WriteLine("State : " + person.State);
-                    Console.WriteLine("Zipcode : " + person.ZipCode);
                 }
             }
         }
