4005b93c5aba0017b764f752d0b7ad599eaa377b
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-21 23:43:35 +0530

    checkDuplicate method is added

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -196,6 +196,7 @@ namespace AddressBookSystem
                 if(People.Contains(person)) 
                 {
                     dict.Add(firstName, People); // it will add the unique contact only and not the duplicate
+                    return;
                 }
             }
         }
